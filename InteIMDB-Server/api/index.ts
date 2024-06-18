77739feea98e05e7774bf49c2dbf70152c886623
Elsa Eriksson <elsa.95.eriksson@gmail.com>
2024-06-18 10:16:29 +0200

    update

--- InteIMDB-Server/api/index.ts
+++ InteIMDB-Server/api/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
-import cors from "cors";
 import { createClient } from "@supabase/supabase-js";
 
 interface IMovie {
