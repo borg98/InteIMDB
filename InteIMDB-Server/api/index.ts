c1c5f70ba8f6125c45236f7178006ed65b7096bf
Elsa Eriksson <elsa.95.eriksson@gmail.com>
2024-06-18 11:40:45 +0200

    update

--- InteIMDB-Server/api/index.ts
+++ InteIMDB-Server/api/index.ts
@@ -149,8 +149,6 @@ const calculateOrderAmount = (items: IMovie[] | null | undefined) => {
 
 app.post("/create-payment-intent", async (req, res) => {
   const { items }: { items: IMovie[] } = req.body;
-  res.json(items);
-  return;
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(items),
     currency: "sek",
