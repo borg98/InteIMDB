6fafa36fc2cd1373a15367fc52fe8d9d5191f502
Elsa Eriksson <elsa.95.eriksson@gmail.com>
2024-06-18 10:28:58 +0200

    update

--- InteIMDB-Server/api/index.ts
+++ InteIMDB-Server/api/index.ts
@@ -30,6 +30,18 @@ app.options("*", cors(corsOptions));
 // app.use(cors({ origin: "*", methods: "*" }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://inte-imdb-l62e.vercel.app"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 dotenv.config();
 const PORT = process.env.PORT;
 const supabaseUrl = process.env.SUPABASE_URL;
